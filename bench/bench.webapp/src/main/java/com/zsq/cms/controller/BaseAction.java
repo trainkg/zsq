e8755046ee1c9c6b6f304d47d111c3d473b66647
垂钓客 <peculiar.1@163.com>
2014-12-16 17:21:39 +0800

    垂钓客

--- bench/bench.webapp/src/main/java/com/zsq/cms/controller/BaseAction.java
+++ bench/bench.webapp/src/main/java/com/zsq/cms/controller/BaseAction.java
@@ -1,5 +1,15 @@
 package com.zsq.cms.controller;
 
+/**
+ *  系统控制器ROOT
+ *  
+ *  设定全局特性
+ * @author zhuyy
+ *
+ */
 public class BaseAction {
+	
+	private IActionBootStrapHander hander;
+	
 	
 }
---
+++ bench/bench.webapp/src/main/java/com/zsq/cms/controller/IActionBootStrapHander.java
@@ -0,0 +1,9 @@
+package com.zsq.cms.controller;
+
+/**
+ * Action 初始化处理器 
+ * @author zhuyy
+ */
+public interface IActionBootStrapHander {
+
+}
