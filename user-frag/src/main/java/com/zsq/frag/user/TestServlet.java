eddc607a8e62cecb0135c63429ea578d8f39c2b4
朱元宇 <peculiar.1@163.com>
2015-03-15 15:52:21 +0800

    user

---
+++ user-frag/src/main/java/com/zsq/frag/user/TestServlet.java
@@ -0,0 +1,32 @@
+/**
+ * Author:  peculiar.1@163.com
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package com.zsq.frag.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * 
+ * @author peculiar.1@163.com
+ * @version $ID: TestServlet.java, V1.0.0 2015年3月15日 下午3:42:33 $
+ */
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("打印请求参数 name"+req.getParameter("name"));
+	}
+}
