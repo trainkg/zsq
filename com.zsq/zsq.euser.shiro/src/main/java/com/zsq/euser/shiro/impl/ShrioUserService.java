2cbe2b6dba8c1525ee9e9bece3e7539d9cfa027c
朱元宇 <peculiar.1@163.com>
2015-04-01 21:14:14 +0800

    Signed-off-by: 朱元宇 <peculiar.1@163.com>

--- com.zsq/zsq.euser.shiro/src/main/java/com/zsq/euser/shiro/impl/ShrioUserService.java
+++ com.zsq/zsq.euser.shiro/src/main/java/com/zsq/euser/shiro/impl/ShrioUserService.java
@@ -2,7 +2,6 @@ package com.zsq.euser.shiro.impl;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,16 +24,7 @@ public class ShrioUserService implements IShrioUserService {
 		UsernamePasswordToken token = new UsernamePasswordToken(name, seEncry.encrypt(user));
 		Subject subject = SecurityUtils.getSubject();
 		if(!subject.isAuthenticated()){
-			Session session1 = subject.getSession(false);
-			System.out.println("A === "+session1);
 			subject.login(token);
-			Session session2 = subject.getSession(false);
-			System.out.println("B==== "+session2);
-			session2.setAttribute("name", "zhuyy");
-		}else{
-			Session session = subject.getSession(false);
-			System.out.println("this session "+ session);
-			System.out.println("get name last " + subject.getSession(false).getAttribute("name"));
 		}
 	}
 
