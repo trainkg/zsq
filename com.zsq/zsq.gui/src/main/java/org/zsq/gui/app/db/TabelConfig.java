a9770f4b9e6ccd966992ed83dcb7c9bc766be2b6
trainkg <peculiar.1@163.com>
2016-04-17 21:28:35 +0800

    Signed-off-by: trainkg <peculiar.1@163.com>

--- com.zsq/zsq.gui/src/main/java/org/zsq/gui/app/db/TabelConfig.java
+++ com.zsq/zsq.gui/src/main/java/org/zsq/gui/app/db/TabelConfig.java
@@ -41,8 +41,21 @@ public class TabelConfig extends JDialog {
 		checkBox4 = new JCheckBox();
 		checkBox5 = new JCheckBox();
 		panel6 = new JPanel();
+		label4 = new JLabel();
+		panel4 = new JPanel();
+		checkBox7 = new JCheckBox();
+		checkBox8 = new JCheckBox();
+		checkBox9 = new JCheckBox();
+		checkBox10 = new JCheckBox();
 		panel7 = new JPanel();
 		panel8 = new JPanel();
+		textField4 = new JTextField();
+		checkBox11 = new JCheckBox();
+		button1 = new JButton();
+		label5 = new JLabel();
+		panel5 = new JPanel();
+		radioButton1 = new JRadioButton();
+		radioButton2 = new JRadioButton();
 		buttonBar = new JPanel();
 		okButton = new JButton();
 		cancelButton = new JButton();
@@ -141,10 +154,40 @@ public class TabelConfig extends JDialog {
 					//======== panel6 ========
 					{
 						panel6.setLayout(new GridBagLayout());
-						((GridBagLayout)panel6.getLayout()).columnWidths = new int[] {0, 0, 0};
-						((GridBagLayout)panel6.getLayout()).rowHeights = new int[] {0, 0, 0, 0};
-						((GridBagLayout)panel6.getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
-						((GridBagLayout)panel6.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+						((GridBagLayout)panel6.getLayout()).columnWidths = new int[] {85, 63, 352, 62, 63, 0};
+						((GridBagLayout)panel6.getLayout()).rowHeights = new int[] {0, 0, 0, 0, 0};
+						((GridBagLayout)panel6.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 1.0E-4};
+						((GridBagLayout)panel6.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 0.0, 1.0E-4};
+
+						//---- label4 ----
+						label4.setText("\u4e1a\u52a1\u529f\u80fd\u5217\u8868\uff1a");
+						panel6.add(label4, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+							new Insets(0, 0, 5, 5), 0, 0));
+
+						//======== panel4 ========
+						{
+							panel4.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+							//---- checkBox7 ----
+							checkBox7.setText("\u65b0\u5efa");
+							panel4.add(checkBox7);
+
+							//---- checkBox8 ----
+							checkBox8.setText("\u5220\u9664");
+							panel4.add(checkBox8);
+
+							//---- checkBox9 ----
+							checkBox9.setText("\u4fee\u6539");
+							panel4.add(checkBox9);
+
+							//---- checkBox10 ----
+							checkBox10.setText("\u67e5\u8be2");
+							panel4.add(checkBox10);
+						}
+						panel6.add(panel4, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+							new Insets(0, 0, 5, 5), 0, 0));
 					}
 					tabbedPane1.addTab("\u4e1a\u52a1\u5c42", panel6);
 
@@ -161,10 +204,50 @@ public class TabelConfig extends JDialog {
 					//======== panel8 ========
 					{
 						panel8.setLayout(new GridBagLayout());
-						((GridBagLayout)panel8.getLayout()).columnWidths = new int[] {0, 0, 0};
+						((GridBagLayout)panel8.getLayout()).columnWidths = new int[] {0, 79, 272, 0, 0};
 						((GridBagLayout)panel8.getLayout()).rowHeights = new int[] {0, 0, 0, 0};
-						((GridBagLayout)panel8.getLayout()).columnWeights = new double[] {0.0, 0.0, 1.0E-4};
+						((GridBagLayout)panel8.getLayout()).columnWeights = new double[] {0.0, 0.0, 0.0, 0.0, 1.0E-4};
 						((GridBagLayout)panel8.getLayout()).rowWeights = new double[] {0.0, 0.0, 0.0, 1.0E-4};
+
+						//---- textField4 ----
+						textField4.setText("\u751f\u6210\u9009\u62e9\u5668\uff1a");
+						panel8.add(textField4, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+							new Insets(0, 0, 5, 5), 0, 0));
+
+						//---- checkBox11 ----
+						checkBox11.setText("\u751f\u6210");
+						panel8.add(checkBox11, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+							new Insets(0, 0, 5, 5), 0, 0));
+
+						//---- button1 ----
+						button1.setText("\u914d\u7f6e\u9009\u62e9\u5668");
+						panel8.add(button1, new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0,
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+							new Insets(0, 0, 5, 0), 0, 0));
+
+						//---- label5 ----
+						label5.setText("\u521b\u5efa\u6570\u636e\u6a21\u5f0f\uff1a");
+						panel8.add(label5, new GridBagConstraints(1, 1, 1, 1, 0.0, 0.0,
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+							new Insets(0, 0, 5, 5), 0, 0));
+
+						//======== panel5 ========
+						{
+							panel5.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+							//---- radioButton1 ----
+							radioButton1.setText("\u5355\u9875\u5e94\u7528");
+							panel5.add(radioButton1);
+
+							//---- radioButton2 ----
+							radioButton2.setText("\u591a\u9875\u5e94\u7528");
+							panel5.add(radioButton2);
+						}
+						panel8.add(panel5, new GridBagConstraints(2, 1, 1, 1, 0.0, 0.0,
+							GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+							new Insets(0, 0, 5, 5), 0, 0));
 					}
 					tabbedPane1.addTab("\u89c6\u56fe", panel8);
 				}
@@ -236,8 +319,21 @@ public class TabelConfig extends JDialog {
 	private JCheckBox checkBox4;
 	private JCheckBox checkBox5;
 	private JPanel panel6;
+	private JLabel label4;
+	private JPanel panel4;
+	private JCheckBox checkBox7;
+	private JCheckBox checkBox8;
+	private JCheckBox checkBox9;
+	private JCheckBox checkBox10;
 	private JPanel panel7;
 	private JPanel panel8;
+	private JTextField textField4;
+	private JCheckBox checkBox11;
+	private JButton button1;
+	private JLabel label5;
+	private JPanel panel5;
+	private JRadioButton radioButton1;
+	private JRadioButton radioButton2;
 	private JPanel buttonBar;
 	private JButton okButton;
 	private JButton cancelButton;
