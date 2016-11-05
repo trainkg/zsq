45766a59242af145ed2a0e4fdbd5873c06f8ff72
triankg <peculiar.1@163.com>
2016-11-05 15:58:56 +0800

    Signed-off-by: triankg <peculiar.1@163.com>

--- com.zsq/zsq.custpl/src/main/webapp/jsapp/view/config/globalConfigView.js
+++ com.zsq/zsq.custpl/src/main/webapp/jsapp/view/config/globalConfigView.js
@@ -18,6 +18,44 @@ define(['jquery','backbone','underscore','text!./globalConfigView-temp.html'],fu
  		//插件相关业务处理
  		initPluginEventHander:function(){
  			var view = this;
+ 			view.$el.find('#dg-model-define').datagrid({
+ 				fit:true,
+ 				border:false,
+ 				toolbar: [
+ 				{
+ 					text:'添加',
+	            	iconCls:'icon-add',
+		            handler:function(){alert('add');}
+		        },'-',
+		        {
+		            text:'配置',
+		            iconCls:'icon-cut',
+		            handler:function(){
+		            	view.gotoConfigPage();
+		            }
+		        },'-',
+		        {
+		            text:'更新',
+		            iconCls:'icon-save',
+		            handler:function(){alert('save');}
+		        },
+		        {
+		            text:'删除',
+		            iconCls:'icon-save',
+		            handler:function(){alert('save');}
+		        }],
+ 			    url:'dbconfig/models',
+ 			    columns:[[
+ 			        {field:'modelName',title:'模块名称',width:80},
+ 			        {field:'description',title:'模块描述',width:120}
+ 			    ]]
+ 			});
+ 		},
+ 		/*
+ 		 * 前往当前选中行的配置页面 
+ 		 */
+ 		gotoConfigPage:function(){
+ 			
  		}
  	});
  	return modelListView;
