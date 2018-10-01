5dfad9680d0f3fb59eb521b5129d8becca88a4af
triankg <peculiar.1@163.com>
2018-10-01 11:40:57 +0800

    pom

--- com.zsq/zsq.party/party.dao/src/main/java/com/zsq/party/dao/PartyDao.java
+++ com.zsq/zsq.party/party.dao/src/main/java/com/zsq/party/dao/PartyDao.java
@@ -1,5 +1,24 @@
 package com.zsq.party.dao;
 
-public class PartyDao {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.zsq.party.modal.Party;
+
+/**
+ * 
+ * @author peculiar.1@163.com
+ * @version $ID: PartyDao.java, V1.0.0 2018年10月1日 上午11:31:37 $
+ */
+public class PartyDao {
+	
+	/**
+	 * 查找所有party
+	 * @return
+	 */
+	List<Party> findAll(){
+		return new ArrayList<Party>();
+	}
+	
+	
 }
